817974ac77fc90cb04cf2e09247cfac95dde9b12
Nteguem roland <nteguemroland@gmail.com>
2024-02-13 14:38:55 +0100

    review

--- src/redux/country/actions.js
+++ src/redux/country/actions.js
@@ -1,8 +1,11 @@
 import {types} from './types';
 
-export const getCountries = () => ({
-  type: types.GET_COUNTRIES_REQUEST,
-});
+export const getCountries = () => {
+  return {
+    type: types.GET_COUNTRIES_REQUEST,
+  };
+};
+
 
 export const getCountryDetails = countries => ({
   type: types.GET_COUNTRY_DETAILS_REQUEST,
--- src/screens/CountriesListScreen.js
+++ src/screens/CountriesListScreen.js
@@ -1,41 +1,65 @@
-import React, { useEffect } from 'react';
-import { View, Text, FlatList, TouchableOpacity } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, ActivityIndicator, StyleSheet } from 'react-native';
 import { useDispatch, connect } from 'react-redux';
 import { getCountries } from '../redux/country/actions'; // Action pour récupérer la liste des pays
+import { Picker } from '@react-native-picker/picker';
 
-const CountriesListScreen = ({ navigation,countries }) => {
+const CountriesListScreen = ({ navigation, countries, loadingCountries }) => {
   const dispatch = useDispatch();
+  const [selectedCountry, setSelectedCountry] = useState('');
 
   useEffect(() => {
     dispatch(getCountries()); // Dispatch de l'action pour récupérer la liste des pays
-    console.log("countries component",countries)
   }, [dispatch]);
 
-  const handleCountryPress = country => {
-    navigation.navigate('CountryDetailsScreen', { country }); // Navigation vers l'écran de détails du pays
+  const handleCountryChange = country => {
+    setSelectedCountry(country);
+    // Vous pouvez ajouter d'autres logiques ici si nécessaire
   };
 
-  return (
-    <View>
-      <FlatList
-  data={countries}
-  keyExtractor={item => item.name.common}
-  renderItem={({ item }) => (
-    <TouchableOpacity onPress={() => handleCountryPress(item)}>
-      <Text>{item.name.common}</Text>
-    </TouchableOpacity>
-  )}
-/>
-
-    </View>
-  );
+  if (loadingCountries) {
+    return (
+      <View style={styles.centeredView}>
+        <ActivityIndicator size="large" color="#0000ff" />
+      </View>
+    );
+  } else {
+    return (
+      <View style={styles.container}>
+        <Picker
+          selectedValue={selectedCountry}
+          onValueChange={handleCountryChange}
+          style={styles.picker}
+        >
+          <Picker.Item label="Select a country" value="" />
+          {countries.map(country => (
+            <Picker.Item key={country.name.common} label={country.name.common} value={country.name.common} />
+          ))}
+        </Picker>
+      </View>
+    );
+  }
 };
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  picker: {
+    width: '80%',
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 const mapStateToProps = ({ CountryReducer }) => ({
   countries: CountryReducer.countries,
+  loadingCountries: CountryReducer.loadingCountries,
 });
 
 export default connect(mapStateToProps)(CountriesListScreen);
-
-
