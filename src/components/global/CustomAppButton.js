365d203c9361e6dde866c34c6b48900efda83b66
Laurel Ndeh <rootndehl@gmail.com>
2024-02-14 16:34:37 +0100

    create custom button with custom styles

---
+++ src/components/global/CustomAppButton.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {Text, TouchableOpacity, StyleSheet} from 'react-native';
+import {colors} from './colors';
+
+const CustomAppButton = ({title, buttonStyle, textStyle, onPress}) => {
+  const passedButtonStyles = Array.isArray(buttonStyle)
+    ? Object.assign({}, ...buttonStyle)
+    : buttonStyle;
+  const passedTextStyles = Array.isArray(textStyle)
+    ? Object.assign({}, ...textStyle)
+    : textStyle;
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      style={[styles.appButtonContainer, {...passedButtonStyles}]}>
+      <Text style={[styles.appButtonText, {...passedTextStyles}]}>{title}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  // ...
+  appButtonContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    elevation: 8,
+    backgroundColor: '#009688',
+    borderRadius: 5,
+    paddingVertical: 5,
+    paddingHorizontal: 5,
+    alignSelf: 'baseline',
+  },
+  appButtonText: {
+    fontSize: 20,
+    color: colors.white,
+    alignSelf: 'center',
+  },
+});
+
+export default CustomAppButton;
