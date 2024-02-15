7fa051f2392af86bd622645cfbf5bb69588121e3
Laurel Ndeh <rootndehl@gmail.com>
2024-02-15 12:53:12 +0100

    update button add props instead of styles

--- src/components/global/CustomAppButton.js
+++ src/components/global/CustomAppButton.js
@@ -2,36 +2,38 @@ import React from 'react';
 import {Text, TouchableOpacity, StyleSheet} from 'react-native';
 import {colors} from './colors';
 
-const CustomAppButton = ({title, buttonStyle, textStyle, onPress}) => {
-  const passedButtonStyles = Array.isArray(buttonStyle)
-    ? Object.assign({}, ...buttonStyle)
-    : buttonStyle;
-  const passedTextStyles = Array.isArray(textStyle)
-    ? Object.assign({}, ...textStyle)
-    : textStyle;
+const CustomAppButton = ({
+  title,
+  onPress,
+  bkgroundColor = '#009688',
+  borderWidth,
+  alignSelf = 'baseline',
+  borderColor = 'blue',
+  textColor = colors.black,
+  textFontSize = 15,
+}) => {
+  const styles = StyleSheet.create({
+    // ...
+    appButtonContainer: {
+      backgroundColor: bkgroundColor,
+      borderRadius: 5,
+      paddingVertical: 5,
+      paddingHorizontal: 5,
+      alignSelf: alignSelf,
+      borderWidth: borderWidth,
+      borderColor: borderColor,
+    },
+    appButtonText: {
+      fontSize: textFontSize,
+      color: textColor,
+      alignSelf: 'center',
+    },
+  });
   return (
-    <TouchableOpacity
-      onPress={onPress}
-      style={[styles.appButtonContainer, {...passedButtonStyles}]}>
-      <Text style={[styles.appButtonText, {...passedTextStyles}]}>{title}</Text>
+    <TouchableOpacity onPress={onPress} style={styles.appButtonContainer}>
+      <Text style={styles.appButtonText}>{title}</Text>
     </TouchableOpacity>
   );
 };
-
-const styles = StyleSheet.create({
-  // ...
-  appButtonContainer: {
-    backgroundColor: '#009688',
-    borderRadius: 5,
-    paddingVertical: 5,
-    paddingHorizontal: 5,
-    alignSelf: 'baseline',
-  },
-  appButtonText: {
-    fontSize: 20,
-    color: colors.white,
-    alignSelf: 'center',
-  },
-});
 
 export default CustomAppButton;
