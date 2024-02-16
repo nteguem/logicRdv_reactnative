d5c44a9ad92b9e235bf9c9399b79ad69190d25d9
Laurel Ndeh <rootndehl@gmail.com>
2024-02-16 13:05:00 +0100

    fetching changes from dev

---
+++ src/components/reusable/PatientdetailsTwo.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import Fontisto from 'react-native-vector-icons/Fontisto';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import LinearGradient from 'react-native-linear-gradient';
+
+const PatientdetailsTwo = () => {
+  return (
+    <View style={styles.parentStyle}>
+      <LinearGradient
+        start={{x: 0, y: 0}}
+        end={{x: 1, y: 0}}
+        colors={['#4c669f', '#3b5998', '#192f6a']}>
+        <View style={styles.row1}>
+          <Fontisto name="first-aid-alt" color="white" size={15} />
+          <Text style={styles.textStyle}>Consultation</Text>
+        </View>
+        <View style={styles.row2}>
+          <View style={styles.row22}>
+            <FontAwesome5 name="user-md" color="white" size={20} />
+            <Text style={styles.textStyle}>Avec Dr Ringuet Nicholas</Text>
+          </View>
+          <View style={styles.row22}>
+            <FontAwesome name="user-circle" color="white" size={20} />
+            <Text style={styles.textStyle}>Pour l47 laurel</Text>
+          </View>
+        </View>
+        <View style={styles.row3}>
+          <Text style={{color: 'white'}}>Le Jeudi 08/02/24 14:40</Text>
+        </View>
+      </LinearGradient>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  parentStyle: {
+    padding: 20,
+    borderRadius: 15,
+    backgroundColor: '#007FA9',
+  },
+  row1: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    backgroundColor: 'gray',
+    borderRadius: 20,
+    alignSelf: 'center',
+  },
+  row2: {
+    alignItems: 'flex-start',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    paddingTop: 15,
+    paddingBottom: 15,
+  },
+  row22: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 3,
+  },
+  row3: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+    backgroundColor: 'gray',
+    borderRadius: 20,
+  },
+  textStyle: {
+    color: 'white',
+    marginLeft: 10,
+    fontSize: 15,
+  },
+});
+
+export default PatientdetailsTwo;
