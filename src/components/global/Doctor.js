d8d7b894a7d541e3e95010ae0f3b8493c8af8c02
Laurel Ndeh <rootndehl@gmail.com>
2024-02-16 08:27:34 +0100

    update doctor add icons

--- src/components/global/Doctor.js
+++ src/components/global/Doctor.js
@@ -5,31 +5,52 @@ import BigText from '../Texts/BigText';
 import SmallText from '../Texts/SmallText';
 import {colors} from './colors';
 import CustomAppButton from './CustomAppButton';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const Doctor = () => {
   return (
     <View style={styles.parentStyles}>
       <View style={styles.myStyles}>
-        <View>
-          <BigText>AAAAAA</BigText>
+        <View style={{flexDirection: 'row'}}>
+          <View style={styles.circleUser}>
+            <Icon name="user-circle" size={50} color={colors.gray} />
+          </View>
+          <View style={{paddingLeft: 10}}>
+            <BigText>Dr Formation</BigText>
+            <Regulartext>Doctor doctor</Regulartext>
+            <SmallText>41 rue de paris</SmallText>
+            <SmallText>75020</SmallText>
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'baseline',
+                justifyContent: 'space-between',
+                width: 100,
+              }}>
+              <Regulartext style={[{color: 'blue'}]}>0176310099</Regulartext>
+              <View style={[styles.circle]}>
+                <Icon name="phone" color="white" size={20} />
+              </View>
+            </View>
+          </View>
         </View>
-        <View>
-          <BigText>Dr Formation</BigText>
-          <Regulartext>Doctor doctor</Regulartext>
-          <SmallText>41 rue de paris</SmallText>
-          <SmallText>75020</SmallText>
-          <Regulartext style={[{color: 'blue'}]}>0176310099</Regulartext>
-        </View>
-        <View>
-          <BigText>AAAAAA</BigText>
+        <View
+          style={{
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            overflow: 'visible',
+          }}>
+          <Icon name="whatsapp" size={25} />
+          <Icon name="whatsapp" size={25} />
+          <Icon name="map-marker" size={25} />
         </View>
       </View>
       <View>
         <CustomAppButton
-          onPress={() => Alert.alert('Button pressed')}
           title="Prendre rdv"
-          buttonStyle={[styles.buttonSyles]}
-          textStyle={[styles.textStyles]}
+          paddingHorizontal={5}
+          paddingVertical={5}
+          borderRadius={5}
         />
       </View>
     </View>
@@ -47,7 +68,7 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     borderColor: colors.primary,
     borderWidth: 1,
-    padding: 5,
+    padding: 10,
   },
   buttonSyles: {
     backgroundColor: 'transparent',
@@ -55,6 +76,28 @@ const styles = StyleSheet.create({
   },
   textStyles: {
     color: 'blue',
+  },
+  circleUser: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: colors.white,
+    borderRadius: 999,
+    width: 55,
+    height: 55,
+    borderWidth: 1,
+    borderColor: colors.gray,
+    marginRight: 5,
+  },
+  circle: {
+    flexDirection: 'row',
+    width: 28,
+    height: 28,
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'blue',
+    marginLeft: 10,
   },
 });
 
