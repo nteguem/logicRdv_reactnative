54ef1ad61ab3190b98138391091a2ede685783a8
Nsanga Mercure Mekinda <mercuremekinda@gmail.com>
2024-02-16 15:26:29 +0100

    appointment component

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -3,24 +3,15 @@ import SmallText from '../components/Texts/SmallText';
 import Regulartext from '../components/Texts/RegularText';
 import BigText from '../components/Texts/BigText';
 import { Alert, StyleSheet, View } from 'react-native';
-import Appointment_Disponibility from '../components/global/Appointment_Disponibility';
-import data from './data';
 
 function Home() {
   return (
     <View>
-      {/* <SmallText style={[{ color: 'magenta' }]}>SmallText</SmallText>
+      <SmallText style={[{ color: 'magenta' }]}>SmallText</SmallText>
       <Regulartext style={[styles.customTextStyle, { color: 'orange' }]}>
         Regulartext
       </Regulartext>
-      <BigText style={[{ color: 'blue' }]}>BigText</BigText> */}
-      {data.map((item, index) => (
-        <Appointment_Disponibility
-          key={index}
-          date={item.date}
-          appointments={item.appointments}
-        />
-      ))}
+      <BigText style={[{ color: 'blue' }]}>BigText</BigText>
       
     </View>
   );
