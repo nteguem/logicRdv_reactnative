15b5065847e8074a712a3860bf265df4f0b5e648
Laurel Ndeh <rootndehl@gmail.com>
2024-02-19 08:28:27 +0100

    add demo

--- src/components/reusable/ProfileOptions.js
+++ src/components/reusable/ProfileOptions.js
@@ -1,12 +1,43 @@
-import React from 'react';
-import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, Switch, Text, TouchableOpacity, View} from 'react-native';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
-const ProfileOptions = ({icon, optionName, onPressAction}) => {
+const ProfileOptions = ({
+  icon,
+  optionName,
+  onPressAction,
+  isRightNotificationToggle,
+}) => {
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+  const myIcon4 = <MaterialIcons name="notifications-on" size={20} />;
   return (
     <TouchableOpacity onPressOut={onPressAction}>
       <View style={styles.container}>
-        {icon && <View style={{paddingHorizontal: 20}}>{icon}</View>}
-        <Text style={{marginLeft: 3}}>{optionName}</Text>
+        {icon && !isEnabled ? (
+          <View style={{paddingHorizontal: 20}}>{icon}</View>
+        ) : (
+          <View style={{paddingHorizontal: 20}}>{myIcon4}</View>
+        )}
+        <Text
+          style={{
+            marginLeft: 3,
+            color: '#244370',
+            fontWeight: 'bold',
+            fontSize: 17,
+          }}>
+          {optionName}
+        </Text>
+        {isRightNotificationToggle && (
+          <Switch
+            style={{flex: 1}}
+            trackColor={{false: '767577', true: '#81bOff'}}
+            thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
+            ios_backgroundColor="#3e3e3e"
+            onValueChange={toggleSwitch}
+            value={isEnabled}
+          />
+        )}
       </View>
     </TouchableOpacity>
   );
