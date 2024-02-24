24304b1bedb746b6c74eac23a0d681750f91ea46
Nteguem roland <nteguemroland@gmail.com>
2024-02-24 12:41:59 +0100

    drawer part 3

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,27 +1,53 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import AuthenticatedNavigator from './AuthenticateNavigator';
 import UnauthenticatedNavigator from './UnAuthenticateNavigator';
-const Stack = createStackNavigator();
-const screenOptions = {gestureEnabled: false, headerShown: false};
+import Home from '../screens/Home';
+import DrawerContent from './DrawerContent';
+import {HeaderIcons} from '../utils/helpers';
 
-const Routes = ({isAuth}) => {
+const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+
+const screenOptions = { gestureEnabled: false, headerShown: false };
+
+const StackScreens = () => {
+  return (
+    <Stack.Navigator screenOptions={screenOptions}>
+    {isAuth ? (
+      <Stack.Screen
+        name="Authenticated"
+        component={AuthenticatedNavigator}
+      />
+    ) : (
+      <Stack.Screen
+      name="UnauthenticatedNavigator"
+      component={UnauthenticatedNavigator}
+    />
+    )}
+  </Stack.Navigator>
+  );
+};
+
+const DrawerScreens = () => {
+  return (
+    <Drawer.Navigator   drawerContent={(props) => <DrawerContent {...props} />} screenOptions = {{ drawerPosition:'right'}}>
+      <Drawer.Screen name="Home" options={screenOptions} component={Home} initialParams={{ left: HeaderIcons.SEARCH, right: HeaderIcons.ACCOUNT  }} />
+    </Drawer.Navigator>
+  );
+};
+
+const Routes = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={screenOptions}>
-        {isAuth ? (
-          <Stack.Screen
-            name="Authenticated"
-            component={AuthenticatedNavigator}
-          />
-        ) : (
-          <Stack.Screen
-          name="UnauthenticatedNavigator"
-          component={UnauthenticatedNavigator}
-        />
-        )}
-      </Stack.Navigator>
+      <Drawer.Navigator
+        drawerContent={props => <DrawerContent {...props} />}
+        screenOptions = {{ drawerPosition:'right'}}>
+        <Drawer.Screen name="DrawerScreens" options={screenOptions} component={DrawerScreens} />
+        <Drawer.Screen name="StackScreens" options={screenOptions} component={StackScreens} />
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 };
