28e6f47511454d3e366157a8b0254b5efc6369a3
franckjor <137257228+franckjor@users.noreply.github.com>
2024-02-26 10:10:18 +0100

    review

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -2,15 +2,9 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import AuthenticatedNavigator from './AuthenticateNavigator';
-<<<<<<< HEAD
-import {HeaderIcons} from '../utils/helpers';
-import Notifications from '../screens/Notification';
-const Stack = createStackNavigator();
-=======
 import UnAuthenticatedNavigator from './UnAuthenticateNavigator';
 import DrawerContent from './DrawerContent';
 const Drawer = createDrawerNavigator();
->>>>>>> 674c7407284d9f6a954df86a026fbba896a03c52
 
 const screenOptions = { drawerPosition: 'right', headerShown: false };
 
@@ -26,19 +20,9 @@ const Routes = ({isAuth}) => {
             component={AuthenticatedNavigator}
           />
         ) : (
-<<<<<<< HEAD
-          <Stack.Screen
-            name="Notifications"
-            component={Notifications}
-            initialParams={{
-              left: HeaderIcons.SEARCH,
-              right: HeaderIcons.ACCOUNT 
-            }}
-=======
           <Drawer.Screen
             name="UnAuthenticatedNavigator"
             component={UnAuthenticatedNavigator}
->>>>>>> 674c7407284d9f6a954df86a026fbba896a03c52
           />
         )}
       </Drawer.Navigator>
