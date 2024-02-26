7cf9117c4972882e61bbc84ceb331af6a592cf41
franckjor <137257228+franckjor@users.noreply.github.com>
2024-02-26 08:55:47 +0100

    ceci est une revision

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -2,11 +2,8 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import AuthenticatedNavigator from './AuthenticateNavigator';
-import Home from '../screens/Home';
-import {HeaderIcons} from '../utils/helpers';
-import Appointments from '../screens/Appointments';
-import PayementForm from '../screens/Payement';
 import ResultatRecherche from '../screens/Resultat';
+
 const Stack = createStackNavigator();
 
 const screenOptions = { drawerPosition: 'right', headerShown: false };
