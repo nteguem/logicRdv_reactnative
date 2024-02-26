49e309515c20725833ce74c3a7179a7c77dc46e8
franckjor <137257228+franckjor@users.noreply.github.com>
2024-02-26 15:35:10 +0100

    review

--- src/components/global/colors.js
+++ src/components/global/colors.js
@@ -3,9 +3,7 @@ export const colors = {
   primary: '#101820',
   white: '#fff',
   black: '#000',
-<<<<<<< HEAD
   blue: '#4b8fb9'
-=======
   black100: '#222732',
   blue: '#4d8fd9',
   blue100: '#244370',
@@ -18,5 +16,4 @@ export const colors = {
   orange: '#ff935c',
   orange100: '#fbe0c4'
 
->>>>>>> 52de0ad568f9bc0a06e5c53c5b9f8a0473671e80
 };
