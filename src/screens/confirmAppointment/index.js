114e9a64ce5d9247b4deb8fa9bb0fec9551bf9da
franckjor <137257228+franckjor@users.noreply.github.com>
2024-02-26 15:33:15 +0100

    review

--- src/screens/confirmAppointment/index.js
+++ src/screens/confirmAppointment/index.js
@@ -1,12 +1,28 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import CustumAlert from '../../components/Alert'
+import { colors } from '../../components/global/colors'
 
-const ConfirmScreen = () => {
+const ConfirmationAppointmentScreen = () => {
   return (
-    <View>
-      <Text>ConfirmScreen</Text>
-    </View>
+    <CustumAlert
+      message="Ceci est une très bonne alerte venant de moi. Votre rendez-vous a été enregistré avec succès."
+      button1={{
+        title: "Prendre un nouveau RDV",
+        textColor: colors.white,
+        backgroundColor: colors.blue,
+        paddingVertical: 10,
+        borderRadius: 10,
+      }}
+      button2={{
+        title: "Quitter",
+        textColor: colors.white,
+        backgroundColor: colors.blue,
+        paddingVertical: 10,
+        borderRadius: 10,
+      }}
+    />
   )
 }
 
-export default ConfirmScreen
+export default ConfirmationAppointmentScreen;
