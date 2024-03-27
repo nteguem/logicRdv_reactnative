8d3d6d7dab62ff04d3676d3294346cfa341a72ac
franckjor <137257228+franckjor@users.noreply.github.com>
2024-03-27 08:56:57 +0100

    review video call

--- src/screens/Video_Call/index.js
+++ src/screens/Video_Call/index.js
@@ -1,31 +1,26 @@
 import React, { useState } from 'react';
-import { SafeAreaView, StyleSheet, ScrollView, View, Text, Dimensions, Button, TouchableOpacity } from 'react-native';
-import { OT, OTSession, OTPublisher, OTSubscriber, OTSubscriberView } from 'opentok-react-native';
+import { StyleSheet, View, Text } from 'react-native';
+import { OTSession, OTPublisher, OTSubscriber, OTSubscriberView } from 'opentok-react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import { useNavigation } from '@react-navigation/native';
 import { colors } from '../../components/global/colors';
+import CustomText from '../../components/global/CustomText';
 
-const dimensions = {
-  width: Dimensions.get('window').width,
-  height: Dimensions.get('window').height,
-};
-
-const mainSubscribersResolution = { width: 1280, height: 720 };
-const secondarySubscribersResolution = { width: 352, height: 288 };
-
-const VideoCall = ({ route, navigation }) => {
+const VideoCall = ( {route} ) => {
+  const navigation = useNavigation();
   const { paiement } = route.params;
   const apiKey = paiement?.tokbox?.apiKey;
   const sessionId = paiement?.tokbox?.sessionId;
   const token = paiement?.tokbox?.token;
-  const doctorName = paiement?.etablissement?.nom;
+  const doctorName = paiement?.etablissement?.nom
+  
 
   const [subscriberIds, setSubscriberIds] = useState([]);
   const [localPublishAudio, setLocalPublishAudio] = useState(true);
   const [localPublishVideo, setLocalPublishVideo] = useState(true);
   const [streamProperties, setStreamProperties] = useState({});
-  const [mainSubscriberStreamId, setMainSubscriberStreamId] = useState(null);
   const [publisherProperties, setPublisherProperties] = useState({ cameraPosition: 'front' });
+  
 
   const sessionEventHandlers = {
     streamCreated: (event) => {
@@ -34,20 +29,28 @@ const VideoCall = ({ route, navigation }) => {
         [event.streamId]: {
           subscribeToAudio: true,
           subscribeToVideo: true,
+          preferredResolution: { width: 1280, height: 720 },
         },
       };
       setStreamProperties(newStreamProperties);
       setSubscriberIds([...subscriberIds, event.streamId]);
     },
     streamDestroyed: (event) => {
-      const updatedSubscriberIds = subscriberIds.filter(id => id !== event.streamId);
-      setSubscriberIds(updatedSubscriberIds);
+      const indexToRemove = subscriberIds.indexOf(event.streamId);
+      if (indexToRemove !== -1) {
+        const newSubscriberIds = [...subscriberIds];
+        newSubscriberIds.splice(indexToRemove, 1);
+        setSubscriberIds(newSubscriberIds);
+      }
       const newStreamProperties = { ...streamProperties };
       delete newStreamProperties[event.streamId];
       setStreamProperties(newStreamProperties);
     },
     error: (error) => {
       console.log('session error:', error);
+    },
+    otrnError: (error) => {
+      console.log('Session otrnError error:', error);
     },
     sessionDisconnected: () => {
       setStreamProperties({});
@@ -101,247 +104,178 @@ const VideoCall = ({ route, navigation }) => {
   };
 
   const endCall = () => {
-    navigation.navigate('Paiement');
-  };
-
-  const handleSubscriberSelection = (subscribers, streamId) => {
-    console.log('handleSubscriberSelection', streamId);
-    let subscriberToSwap = subscribers.indexOf(streamId);
-    let currentSubscribers = subscribers;
-    let temp = currentSubscribers[subscriberToSwap];
-    currentSubscribers[subscriberToSwap] = currentSubscribers[0];
-    currentSubscribers[0] = temp;
-    const newStreamProps = { ...streamProperties };
-    for (let i = 0; i < currentSubscribers.length; i += 1) {
-      if (i === 0) {
-        newStreamProps[currentSubscribers[i]] = {
-          ...streamProperties[currentSubscribers[i]],
-        };
-        newStreamProps[currentSubscribers[i]].preferredResolution = mainSubscribersResolution;
-      } else {
-        newStreamProps[currentSubscribers[i]] = {
-          ...streamProperties[currentSubscribers[i]],
-        };
-        newStreamProps[currentSubscribers[i]].preferredResolution = secondarySubscribersResolution;
-      }
-    }
-    console.log('mainSubscriberStreamId', streamId);
-    console.log('streamProperties#2', newStreamProps);
-    setMainSubscriberStreamId(streamId);
-    setStreamProperties(newStreamProps);
+      navigation.navigate('Paiement');
   };
 
   const renderSubscribers = (subscribers) => {
-    console.log('renderSubscribers', subscribers);
-    console.log('subscriberIds', subscriberIds);
-    console.log('mainSubscriberStreamId', mainSubscriberStreamId);
-  
-    // Déterminer le flux principal et le déplacer au début de la liste pour qu'il soit affiché en premier
-    if (mainSubscriberStreamId) {
-      subscribers = subscribers.filter(sub => sub !== mainSubscriberStreamId);
-      subscribers.unshift(mainSubscriberStreamId);
-    }
-  
-    // Rendre les vues des flux vidéo
-    return (
-      <>
-        {/* Affichage du texte du médecin en attente de réponse si personne n'a encore rejoint la vidéoconférence */}
-        {subscribers.length === 0 && (
-          <View style={styles.CallName}>
-            <Text style={styles.DR}>{doctorName}</Text>
-            <Text style={styles.wait}>En attente de réponse...</Text>
-          </View>
-        )}
-  
-        {/* Affichage des flux vidéo */}
-        {subscribers.map((streamId, index) => (
-          <View
+    if (subscribers.length > 0) {
+      const streamId = subscribers[0]; 
+      return (
+          <OTSubscriberView
+            streamId={streamId}
             key={streamId}
-            style={[
-              styles.subscriberContainer,
-              index === 0 ? styles.mainSubscriberContainer : styles.secondarySubscriberContainer,
-            ]}>
-            <TouchableOpacity
-              onPress={() => handleSubscriberSelection(subscribers, streamId)}>
-              <OTSubscriberView
-                streamId={streamId}
-                style={{ width: '100%', height: '100%' }}
-              />
-            </TouchableOpacity>
-          </View>
-        ))}
-      </>
-    );
+            style={{width: '100%', height: '70%', }}
+          />
+      );
+    } else {
+      return (
+        <View style={styles.CallName}>
+          <Text style={styles.DR}> {doctorName} </Text>
+          <Text style={styles.wait}> En attente de réponse... </Text>
+        </View>
+      );
+    }
   };
   
+
   const videoView = () => {
     return (
-      <>
-        <View style={styles.fullView}>
+        <>
           <View style={styles.chiffrement}>
             <Icon
-              style={{ color: colors.gray500 }}
-              name="lock-outline"
+              style={{ color: colors.gray }}
+              name='lock-outline'
             />
-            <Text style={{ color: colors.gray500 }}>Chiffré de bout en bout</Text>
+            <CustomText fontSize={12} style={{ color: colors.gray }}> Chiffré de bout en bout</CustomText>
           </View>
-          <OTSession
-            apiKey={apiKey}
-            sessionId={sessionId}
-            token={token}
-            eventHandlers={sessionEventHandlers}
-            options={{ enableStereoOutput: true }}>
-            <OTPublisher
-              properties={publisherProperties}
-              eventHandlers={publisherEventHandlers}
-              style={styles.publisherStyle}
-            />
-            <OTSubscriber
-              style={{ height: dimensions.height, width: dimensions.width }}
-              eventHandlers={subscriberEventHandlers}
-              streamProperties={streamProperties}>
-              {renderSubscribers}
-            </OTSubscriber>
-          </OTSession>
-        </View>
-
-        <View style={styles.buttonView}>
-          <View style={styles.circularIconContainer}>
-            <Icon
-              style={styles.allicon}
-              name={localPublishAudio ? 'mic' : 'mic-off'}
-              onPress={toggleAudio}
-            />
+          <View style={styles.fullView}>
+            <OTSession
+              apiKey={apiKey}
+              sessionId={sessionId}
+              token={token}
+              eventHandlers={sessionEventHandlers}>
+              <OTPublisher
+                properties={publisherProperties}
+                eventHandlers={publisherEventHandlers}
+                style={styles.publisherStyle}
+              />
+              <OTSubscriber
+                eventHandlers={subscriberEventHandlers}
+                streamProperties={streamProperties}>
+                {renderSubscribers}
+              </OTSubscriber>
+            </OTSession>
           </View>
-          <View style={styles.circularIconContainer}>
-            <Icon
-              name="flip-camera-ios"
-              style={styles.allicon}
-              onPress={toggleCamera}
-            />
+  
+          <View style={styles.buttonView}>
+            <View style={styles.circularIconContainer}>
+              <Icon
+                style={styles.allicon}
+                name={localPublishAudio ? 'mic' : 'mic-off'}
+                onPress={toggleAudio}
+              />
+            </View>
+            <View style={styles.circularIconContainer}>
+              <Icon
+                name="flip-camera-ios"
+                style={styles.allicon}
+                onPress={toggleCamera}
+                size = "10"
+              
+              />
+            </View>
+            <View style={styles.circularIconContainer}>
+              <Icon
+                style={styles.allicon}
+                name={localPublishVideo ? 'videocam' : 'videocam-off'}
+                onPress={toggleVideo}
+              />
+            </View>
+            <View style={styles.phoneend }>
+              <Icon
+                style={styles.allicon }
+                name="call-end"
+                onPress={endCall}
+              />
+            </View>
           </View>
-          <View style={styles.circularIconContainer}>
-            <Icon
-              style={styles.allicon}
-              name={localPublishVideo ? 'videocam' : 'videocam-off'}
-              onPress={toggleVideo}
-            />
-          </View>
-          <View style={styles.phoneend}>
-            <Icon
-              style={styles.allicon}
-              name="call-end"
-              onPress={endCall}
-            />
-          </View>
-        </View>
-      </>
-    );
+        </>
+      );
   };
 
-  return videoView();
+ 
+
+  return  videoView() ;
 };
 
 const styles = StyleSheet.create({
-  buttonView: {
-    height: 50,
-    backgroundColor: 'transparent',
-    display: 'flex',
-    width: '100%',
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignContent: 'center',
-    marginVertical: 5,
-  },
-
-  fullView: {
-    flex: 1,
-  },
-  scrollView: {},
-  footer: {
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-  publisherStyle: {
-    width: 100,
-    height: 150,
-    position: 'absolute',
-    bottom: 70,
-    right: 5,
-    zIndex: 5,
-  },
-  mainSubscriberStyle: {
-    height: (dimensions.height * 3) / 4 - 50,
-  },
-  secondarySubscriberContainer: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'transparent',
-  },
-  secondarySubscribers: {
-    height: dimensions.height / 4,
-  },
-  circularIconContainer: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: 50,
-    height: 50,
-    borderRadius: 35,
-    backgroundColor: '#C0C0C0',
-    marginHorizontal: 10,
-  },
-  phoneend: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: 50,
-    height: 50,
-    borderRadius: 35,
-    backgroundColor: '#ff0000',
-    marginHorizontal: 10,
-  },
-  allicon: {
-    paddingHorizontal: 4,
-    paddingVertical: 2,
-    color: '#fff',
-    fontSize: 30,
-    height: 35,
-    width: 35,
-  },
-  chiffrement: {
-    position: "absolute",
-    left: "25%",
-    backgroundColor: "transparent",
-    justifyContent: "center",
-    alignItems: "center",
-    flexDirection: "row",
-    marginVertical: 4,
-    color: colors.gray500
-  },
-  CallName: {
-    justifyContent: "center",
-    alignItems: "center",
-    flex: 1,
-  },
-  DR: {
-    color: "#000",
-    fontSize: 18,
-    fontWeight: "bold",
-  },
-  wait: {
-    fontSize: 13,
-    color: colors.gray500
-  }
-});
+    buttonView: {
+      height: 50,
+      backgroundColor: 'transparent', 
+      display: 'flex',
+      width: '100%',
+      position: 'absolute',
+      bottom: 10,
+      left: 0,
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignContent: 'center',
+      marginVertical:5,
+    },
+   
+    fullView: {
+      flex: 1,
+    },
+    publisherStyle: {
+      position: 'absolute',
+      width: 150,
+      height: 200,
+      bottom:70,
+      right: 5,
+  
+    },
+   
+    circularIconContainer: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      width: 50,
+      height: 50,
+      borderRadius: 35,
+      backgroundColor:  '#C0C0C0',
+      marginHorizontal: 10,
+    },
+    phoneend :{
+      justifyContent: 'center',
+      alignItems: 'center',
+      width: 50,
+      height: 50,
+      borderRadius: 35,
+      backgroundColor:  '#ff0000',
+      marginHorizontal: 10,
+    },
+    allicon:{
+      paddingHorizontal:4,
+      paddingVertical:2,
+      color: '#fff',
+      fontSize: 30,
+    },
+    chiffrement:{
+      position:"absolute",
+      borderRadius:10,
+      top:20,
+      backgroundColor:colors.white,
+      justifyContent:"center",
+      alignItems:"center",
+      flexDirection:"row",
+      marginVertical:4,
+      color:colors.gray500,
+      height:20,
+      width:"100%"
+    },
+    CallName:{
+      justifyContent:"center",
+      alignItems:"center",
+      flex:1,
+    },
+    DR:{
+      color: "#000",
+      fontSize:18,
+      fontWeight:"bold",
+    },
+    wait:{
+      fontSize:13,
+      color:colors.gray500
+    }
+  });
 
 export default VideoCall;
