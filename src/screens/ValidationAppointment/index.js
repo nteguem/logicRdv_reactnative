544c9be4dd973c82dbc42f67afc23539b04ff1c3
Nsanga Mercure Mekinda <mercuremekinda@gmail.com>
2024-04-04 14:10:02 +0100

    revue

--- src/screens/ValidationAppointment/index.js
+++ src/screens/ValidationAppointment/index.js
@@ -343,16 +343,17 @@ const ValidationAppointment = ({ route, session, data, isLoadingAppointment, par
                 <CardField
                 postalCodeEnabled={true}
                 placeholders={{
-                  number: '4242 4242 4242 4242',
+                  number: 'XXXX XXXX XXXX XXXX',
+                  expiration: 'MM/YY',
+                  cvc: 'CVC',
                 }}
                 cardStyle={{
-                  backgroundColor: '#FFFFFF',
+                  // backgroundColor: 'none',
                   textColor: '#000000',
+                  
                 }}
                 style={{
-                  width: '100%',
                   height: 50,
-                  marginVertical: 30,
                 }}
                 onCardChange={(cardDetails) => {
                   console.log('cardDetails', cardDetails);
