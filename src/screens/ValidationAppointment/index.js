598fd035c4c1ea8a3e08d366bbcc52e4fe1709f8
franckjor <137257228+franckjor@users.noreply.github.com>
2024-09-17 10:56:35 +0100

    review dateTimePicker

--- src/screens/ValidationAppointment/index.js
+++ src/screens/ValidationAppointment/index.js
@@ -87,7 +87,7 @@ const FloatingLabelInput = ({
       <View>
       {editable === false ? (
         <Pressable onPress={showDatePicker} style={[styles.input, styles.dateInput]}>
-          <Text>{value ? String(value) : "jj/mm/aaaa"}</Text>
+          <Text style={{color:colors.black}}>{value ? String(value) : "jj/mm/aaaa"}</Text>
         </Pressable>
       ) : (
         <TextInput
